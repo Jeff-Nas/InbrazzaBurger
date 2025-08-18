f4957c60d0d9d40878de204ab334496bbd61fdcd
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-08-17 21:36:43 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './styles.css'
+
+function App() {
+
+  return (
+    <main className='min-h-[80vh] p-4'>
+      <Routes>
+        <Route path='"/' element />
+      </Routes>
+    </main>
+  )
+}
+
+export default App
