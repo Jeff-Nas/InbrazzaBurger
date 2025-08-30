479d13b4db038ea9f255fb42ccd83f56fdb27ef8
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-08-29 22:30:02 -0300

    Protecting new API key

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,9 +2,10 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export default function Map() {
     const center = { lat: -3.8767460144116748, lng: -38.599790832938616 }
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
     return (
-        <LoadScript googleMapsApiKey="AIzaSyAAELhEIKuaGwjD55MQ43V9_HfBku4rP1Q">
+        <LoadScript googleMapsApiKey={apiKey}>
             <GoogleMap
                 mapContainerStyle={{ width: "100%", height: "200px" }}
                 center={center}
