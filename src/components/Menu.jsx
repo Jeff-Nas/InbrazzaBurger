f5e9c03a55a1c8724ce437532914bc24ca3cc301
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-09-03 17:11:39 -0300

    Editing Menu animation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 
 export function Menu() {
+
+    const [active, setActive] = useState(null)
+
     const menuItems = [
         { label: 'Entradas', id: 'entradas' },
         { label: 'Combos', id: 'combos' },
@@ -9,13 +14,28 @@ export function Menu() {
         { label: 'Bebidas', id: 'bebidas' },
     ];
 
+    const handleClick = (id) => {
+        console.log('Item ativo:', id)
+    }
     return (
         <nav className="flex gap-2 p-2 overflow-x-auto whitespace-nowrap no-scrollbar my-3">
-            {menuItems.map(item => (
-                <a key={item.id} href={`#${item.id}`}>
-                    <li className="list-none">{item.label}</li>
-                </a>
-            ))}
+            <ul className="flex gap-5">
+                {menuItems.map((item) => (
+                    <li key={item.id} className="list-none font-medium text-[13px]">
+                        <a
+                            //href={`#${item.id}`} 
+                            onClick={() => setActive(item.id)}
+
+                            className={`block py-1 text-gray-800 ${active === item.id
+                                ? " text-red-700 border-b-2 border-red-700"
+                                : ""
+                                }`}
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </nav>
-    )
+    );
 }
